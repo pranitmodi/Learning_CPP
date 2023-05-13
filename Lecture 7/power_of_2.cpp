30ec0ec8a787e59e6c42de6ab17163973e646a33
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-05-13 16:13:55 +0530

    13TH MAY

---
+++ Lecture 7/power_of_2.cpp
@@ -0,0 +1,28 @@
+// 231. Power of Two
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+    
+    double d = 0;
+    int count = 0;
+    while(d <= n)
+    {
+        d = pow(2,count);
+        if(n == d)
+        {
+            cout << "Yes a power of two: 2^" << count << " = " << n << endl;
+            return 0;
+        }
+        count ++;
+    }
+    if(d>n)
+        cout << "Not a power of two" << endl;
+    return 0;
+}
