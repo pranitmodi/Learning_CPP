f1938756a97c9d3aabc90a5cfefe7273a2338f9d
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-05-30 22:51:34 +0530

    30th may

--- Lecture 10/unique_no_of_occurence.cpp
+++ Lecture 10/unique_no_of_occurence.cpp
@@ -17,7 +17,6 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> ans;
     int size = sizeof(arr)/sizeof(int);
 
     cout << "Array elements: " << endl;
@@ -27,6 +26,7 @@ int main()
     }
     cout << endl;
     
+    // Sorting the array
     for(int i=0; i<s; i++)
     {
         int t=arr[i];
@@ -50,6 +50,8 @@ int main()
     }
     cout << endl;
 
+    vector<int> ans; //Another vector to store the frequency of the elements
+
     int i=0;
     while(i < s)
     {
@@ -58,26 +60,27 @@ int main()
         {
             if(arr[i] == arr[j])
             {
-                count++;
+                count++; //while same element
             }
             else
             {
-                break;
+                break; //as soon as different element
             }
         }
         ans.push_back(count);
-        i=i+count;
+        i=i+count; //to the next unique element
     }
+
     size = ans.size();
-    sort(ans.begin(),ans.end());
+    sort(ans.begin(),ans.end()); // sorting the vector
     for(int i=0; i<size-1; i++)
     {
-        if(ans[i] == ans[i+1])
+        if(ans[i] == ans[i+1]) // if two elements are same ie two numbers in the original array have the same frequency
         {
             cout << "false";
             return 0;
         }
     }
-    cout << "true";
+    cout << "true"; // if false hasn't been returned till now then true
     return 0;
 }
