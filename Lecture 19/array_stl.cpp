81b6dd093c2ede7e2baddc1d3585401395ee0def
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-06-12 12:57:09 +0530

    12th june

--- Lecture 19/array_stl.cpp
+++ Lecture 19/array_stl.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     array <int,5> a;
-    cout << a.empty() << endl; //returns 0
     cout << "Enter elements: " << endl;
 
     int size = a.size();
@@ -19,8 +18,6 @@ int main()
         cout << a[i] << " " << endl;
     }
     cout << endl; 
-
-    cout << a.empty() << endl; //returns 0
 
     return 0;
 }
