979a28914d77a9d69351773cc50a8fcf1b69ce68
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-06-16 00:42:33 +0530

    16th june

--- Lecture 22/LeetCode/remove_all_adjacent_duplicates.cpp
+++ Lecture 22/LeetCode/remove_all_adjacent_duplicates.cpp
@@ -2,6 +2,43 @@
 using namespace std;
 int main()
 {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    // Faster Solution
+    // string temp = "";
+    // for(int i =0; i<s.length(); i++)
+    // {
+    //     temp.push_back(s[i]);
+    //     if(temp.size() >= 2 && temp[temp.length() -1] == temp[temp.length() -2])
+    //     {
+    //     temp.pop_back();
+    //     temp.pop_back(); 
+    //     }
+    // }
+    // return temp;
+
+    int c = 1;
+    while(c == 1)
+    {
+        c = 0;
+        if(s.length() == 0)
+            cout << "Done?" << endl;
+        for(int i=0; i<(s.length()-1); i++)
+        {
+            cout << s[i] << "i:" << i << endl;
+            if(s[i] == s[i+1])
+            {
+                c=1;
+                s.erase(i,2);
+                cout << "New: " << s << endl;
+                break;
+            }
+        }
+    }
+
+    cout << "Final: " << s << endl;
 
     return 0;
 }
