5cedb829771ed197f7f3325db99adb757302e27f
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-06-23 14:38:03 +0530

    okay

--- Lecture 34/insertionSort_recursion.cpp
+++ Lecture 34/insertionSort_recursion.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 void insertionSort1(int arr[], int n)
 {
-
+    
 }
 void insertionSort2(int arr[], int n)
 {
-
+    for(int i=1; i<n; i++)
+    {
+        int j = i;
+        while(j>0)
+        {
+            if(arr[j] < arr[j-1])
+                swap(arr[j], arr[j-1]); //traditionally they shift each and in the last put arr[i] to its place, but i am swapping so same logic
+            else
+                break;
+            j--;
+        }
+    }
 }
 int main()
 {
@@ -21,7 +32,7 @@ int main()
         cin >> arr[i];
     }
 
-    insertionSort1(arr, n);
+    insertionSort2(arr, n);
 
     cout << "Array elements: " << endl;
     for(int i=0; i<n; i++)
