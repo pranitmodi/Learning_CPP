39f0e2d0a1b1eff7ff1cb330472396944eeee4fb
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-07-16 23:58:43 +0530

    hmm

--- Lecture 64/zigzag_Traversal.cpp
+++ Lecture 64/zigzag_Traversal.cpp
@@ -61,3 +61,42 @@ vector<int> zigZagTraversal(Node* root)
     
     return ans;
 }
+
+// Approach 2 - Better
+vector<int> zigZagTraversal(Node* root)
+{
+    vector<int> result;
+    if(root == NULL)
+        return result;
+
+    queue<Node*> q;
+    q.push(root);
+
+    bool leftToRight = true;
+    
+    while(!q.empty())
+    {
+        int size = q.size();
+        vector<int> ans(size);
+
+        for(int i=0; i<size; i++)
+        {
+            Node *temp = q.front();
+            q.pop();
+
+            int index = leftToRight ? i : (size - i - 1);
+            ans[index] = temp->data;
+
+            if(temp->left != NULL)
+                q.push(temp->left);
+            
+            if(temp->right != NULL)
+                q.push(temp->right);
+        }
+
+        leftToRight = !leftToRight;
+        for(auto i: ans)
+            result.push_back(i);
+    }
+    return result;
+}
