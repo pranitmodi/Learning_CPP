39445804bc83db1d6a0c8e986ff6741dca4e1c68
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-08-14 18:46:15 +0530

    okay

---
+++ #Practice/NeetCode/Sliding Window/best_time_to_buy_sell_stock.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution 
+{
+    public:
+        int maxProfit(vector<int>& prices)
+        {
+            int maxi = prices[0];
+            int mini = prices[0];
+            int ind_mini = 0;
+            int ind_maxi = 0;
+            int ans = 0;
+
+            for(int i=0; i<prices.size(); i++)
+            {
+                if(prices[i] < mini)
+                {
+                    mini = prices[i];
+                    ind_mini = i;
+                }
+
+                if(prices[i] > maxi || (ind_maxi <= ind_mini && prices[i] > mini))
+                {
+                    maxi = prices[i];
+                    ind_maxi = i;
+                }
+
+                if(ind_maxi > ind_mini && (maxi-mini) > ans)
+                {
+                    ans = maxi - mini;
+                }
+            }
+
+            return ans;
+        }
+};
