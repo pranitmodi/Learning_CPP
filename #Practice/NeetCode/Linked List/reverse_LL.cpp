130a2a87589fd65899f83614863f1939706395be
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-08-15 21:36:08 +0530

    done

---
+++ #Practice/NeetCode/Linked List/reverse_LL.cpp
@@ -0,0 +1,53 @@
+// leetCode - 206
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode* reverseList(ListNode* head) 
+{
+    // better Space Complexity
+    if(head == NULL)
+        return NULL;
+    if(head->next == NULL)
+        return head;
+
+    ListNode *temp = head->next;
+    head->next = NULL;
+    if(temp->next == NULL)
+    {
+        temp->next = head;
+        return temp;
+    }
+    while(temp->next != NULL)
+    {
+        ListNode *t = temp->next;
+        temp->next = head;
+        head = temp;
+        temp = t;
+    }
+    temp->next = head;
+    return temp;
+}
+
+ListNode* reverseList1(ListNode* head) 
+{
+    // better approach
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+    ListNode* Nxt=NULL;
+    while(curr!=NULL)
+    {
+        Nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=Nxt;
+    }
+    return prev;
+}
