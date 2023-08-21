53a880b844aa7e1d29f4eee74c74d2f6b81969c2
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-08-21 20:40:12 +0530

    pata mnahi bhaiaiiiii

---
+++ #Practice/NeetCode/Linked List/kkk.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int n, int k)
+{
+    int n,k;
+    vector<int> v(n-1,1);
+    cin >> n;
+    cin >> k;
+    while(n == 1)
+    {
+        for(int i=1; i<=k; i++)
+        {
+            if(k > n)
+            {
+                int s = k%n;
+                
+            }
+        }
+    }
+}
