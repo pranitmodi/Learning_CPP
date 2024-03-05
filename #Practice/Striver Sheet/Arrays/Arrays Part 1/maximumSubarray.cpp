a3373404c2e523f883fdf83acf0ed5a3e16d04b2
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2024-03-05 13:54:31 +0530

    yuss

---
+++ #Practice/Striver Sheet/Arrays/Arrays Part 1/maximumSubarray.cpp
@@ -0,0 +1,19 @@
+// 53
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int maxi = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            sum += nums[i];
+            maxi = max(maxi,sum);
+            if(sum < 0)
+                sum = 0;
+        }    
+        return maxi;
+    }
+};
