b3746d2e69525c4cdea0681c76176f3b02057299
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2024-03-10 09:39:06 +0530

    yess

--- #Practice/Company Specific/Signify/ddd.java
+++ #Practice/Company Specific/Signify/ddd.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 public class ddd {
-    private static final long MOD = 1000000007;
+    // private static final long MOD = 1000000007;
+
+    private static final int MOD = 1000000007;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        long sum = 0;
-
-        for (int i = 1; i <= N; i += 2) {
-            sum += i;
-            sum %= MOD; // To handle the large output, take modulo at each step
-        }
-
-        System.out.println(sum);
+        sc.close();
+        
+        int totalSum = calculateSumFreeSubsetSum(N);
+        System.out.println(totalSum);
     }
+
+    private static int calculateSumFreeSubsetSum(int N) {
+        long subsetCount = (N + 1L) / 2L;
+        long sum = subsetCount * subsetCount % MOD;
+        return (int)sum;
+}
+
+
+
 }
