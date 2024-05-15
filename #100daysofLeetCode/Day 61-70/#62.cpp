47cc56358ba36dc462c8c100c030290ea063f8cf
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2024-05-15 20:48:53 +0530

    progress day 62

---
+++ #100daysofLeetCode/Day 61-70/#62.cpp
@@ -0,0 +1,19 @@
+// 1662. Check If Two String Arrays are Equivalent
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        string str1 = "";
+        string str2 = "";
+
+        for(string s:word1)
+            str1 += s;
+
+        for(string s:word2)
+            str2 += s;
+
+        return (str1 == str2);
+    }
+};
