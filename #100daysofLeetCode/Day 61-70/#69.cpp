3ceadb4058eeba59cc0fb81b79d7775ea83f73ca
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2024-05-30 17:58:47 +0530

    progress day 69

--- #100daysofLeetCode/Day 61-70/#69.cpp
+++ #100daysofLeetCode/Day 61-70/#69.cpp
@@ -9,7 +9,6 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
 class Solution {
 public:
     void solve(vector<int> &v, TreeNode *root)
