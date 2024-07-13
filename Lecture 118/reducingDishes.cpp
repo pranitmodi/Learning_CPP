f168ee81b7ae157dececc708a0fbb8de3c02b228
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2024-07-14 01:07:46 +0530

    yes

--- Lecture 118/reducingDishes.cpp
+++ Lecture 118/reducingDishes.cpp
@@ -1,4 +1,4 @@
-// 
+// 1402. Reducing Dishes
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -28,4 +28,111 @@ int maxSatisfaction(vector<int>& satisfaction)
     vector<int> v;
     sort(satisfaction.begin(),satisfaction.end());
     return solve(satisfaction,0,v);
+}
+
+// Recursion (Better approach)
+int solve(vector<int>& satisfaction, int i, int sum, int t)
+{
+    if(i==satisfaction.size())
+        return sum;
+
+    int a = solve(satisfaction,i+1,sum,t);
+    sum += (t*satisfaction[i]);
+    int b = solve(satisfaction,i+1,sum,t+1);
+
+    return max(a,b);
+}
+int maxSatisfaction(vector<int>& satisfaction) 
+{
+    sort(satisfaction.begin(),satisfaction.end());
+    return solve(satisfaction,0,0,1);
+}
+
+// Recursion + Memorization
+int solve(vector<int>& satisfaction, int i, int sum, int t, vector<vector<int>> &dp)
+{
+    if(i==satisfaction.size())
+        return sum;
+
+    if(dp[i][t] != -1)
+        return dp[i][t];
+
+    int a = solve(satisfaction,i+1,sum,t,dp);
+    sum += (t*satisfaction[i]);
+    int b = solve(satisfaction,i+1,sum,t+1,dp);
+
+    return dp[i][t] = max(a,b);
+}
+int maxSatisfaction(vector<int>& satisfaction) 
+{
+    sort(satisfaction.begin(),satisfaction.end());
+    int n = satisfaction.size();
+    vector<vector<int>> dp(n,vector<int>(n+1,-1));
+    return solve(satisfaction,0,0,1,dp);
+}
+
+// Tabulation
+int maxSatisfaction(vector<int>& satisfaction) 
+{
+    sort(satisfaction.begin(),satisfaction.end());
+    int n = satisfaction.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int j=i; j>=0; j--)
+        {
+            int a = dp[i+1][j];
+            int b = ((j+1)*satisfaction[i]) + dp[i+1][j+1]; //this (j+1) is a mystry
+
+            dp[i][j] = max(a,b);
+        }
+    }
+
+    return dp[0][0];
+}
+
+// Space Optimization - O(2*n)
+int maxSatisfaction(vector<int>& satisfaction) 
+{
+    sort(satisfaction.begin(),satisfaction.end());
+    int n = satisfaction.size();
+    // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    vector<int> curr(n+1,0);
+    vector<int> prev(n+1,0);
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int j=i; j>=0; j--)
+        {
+            int a = prev[j];
+            int b = ((j+1)*satisfaction[i]) + prev[j+1];
+
+            curr[j] = max(a,b);
+        }
+        prev = curr;
+    }
+
+    return prev[0];
+}
+
+// Space Optimization further - O(n)
+int maxSatisfaction(vector<int>& satisfaction) 
+{
+    sort(satisfaction.begin(),satisfaction.end());
+    int n = satisfaction.size();
+    vector<int> prev(n+1,0);
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int j=0; j<=i; j++)
+        {
+            int a = prev[j];
+            int b = ((j+1)*satisfaction[i]) + prev[j+1];
+
+            prev[j] = max(a,b);
+        }
+    }
+
+    return prev[0];
 }
---
+++ Lecture 119/longestIncreasingSubsequence.cpp
@@ -0,0 +1,87 @@
+// 300. Longest Increasing Subsequence
+#include <bits/stdc++.h>
+using namespace std;
+
+// Recusrion
+int solve(vector<int>& nums, int i, int l)
+{
+    if(i == nums.size())
+        return 0;
+
+    int a = solve(nums,i+1,l);
+    int b = 0;
+    if(nums[i]>l)
+        b = solve(nums,i+1,nums[i]) + 1;
+
+    return max(a,b);
+}
+int lengthOfLIS(vector<int>& nums) {
+    return solve(nums,0,INT_MIN);
+}
+
+// Recusrion + Memorization
+int solve(vector<int>& nums, int i, int l, vector<vector<int>> &dp)
+{
+    if(i == nums.size())
+        return 0;
+
+    if(dp[i][l+1] != -1)
+        return dp[i][l+1];
+
+    int a = solve(nums,i+1,l,dp);
+    int b = 0;
+    if(l==-1 || nums[i]>nums[l])
+        b = solve(nums,i+1,i,dp) + 1;
+
+    return dp[i][l+1] = max(a,b); //+1 beacuse we start l with -1.
+}
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+    return solve(nums,0,-1,dp);
+}
+
+// Tabulation
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n+1,vector<int> (n+1,0)); // initially all 0
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int l=i-1; l>=-1; l--)
+        {
+            int a = dp[i+1][l+1]; //l+1
+            int b = 0;
+            if(l==-1 || nums[i]>nums[l])
+                b = dp[i+1][i+1] + 1; //i+1
+
+            dp[i][l+1] = max(a,b);
+        }
+    }
+
+    return dp[0][0];
+}
+
+// Space Optimization
+int lengthOfLIS(vector<int>& nums) 
+{
+    int n = nums.size();
+    vector<int> curr(n+1,0);
+    vector<int> prev(n+1,0);
+
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int l=i-1; l>=-1; l--)
+        {
+            int a = prev[l+1]; //l+1
+            int b = 0;
+            if(l==-1 || nums[i]>nums[l])
+                b = prev[i+1] + 1; //i+1
+
+            curr[l+1] = max(a,b);
+        }
+        prev = curr;
+    }
+
+    return prev[0];
+}
