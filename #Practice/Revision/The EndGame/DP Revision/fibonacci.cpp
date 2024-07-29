2df5cb916bc72c71d50ea2f455fdf95a2fadf898
Pranit Modi <pranitmodi007@gmail.com>
2024-07-29 10:11:34 +0530

    yess

---
+++ #Practice/Revision/The EndGame/DP Revision/fibonacci.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Recusrion + Mem
+int solve(int n, vector<int> &dp)
+{
+    if(n == 1)
+        return 0;
+
+    if(n == 2)
+        return 1;
+
+    if(dp[n] != -1)
+        return dp[n];
+
+    return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+}
+
+// Tabulation
+int tab(int n)
+{
+    vector<int> dp(n+1,0);
+    dp[1] = 1;
+    for(int i=2; i<=n; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n-1];
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    vector<int> dp(n+1,-1);
+    // cout << "Ans: " << solve(n,dp) << endl;
+
+    cout << "Ans: " << tab(n) << endl;
+}
